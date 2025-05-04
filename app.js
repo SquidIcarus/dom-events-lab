1d384dcb5874869f30dda3f1d7bc17c477fb5ef3
User Name <landonisaiah@gmail.com>
2025-05-04 16:28:43 -0700

    logs number button innerText with eventListener

--- app.js
+++ app.js
@@ -1,9 +1,17 @@
 /*-------------------------------- Constants --------------------------------*/
+const numButtons = document.querySelectorAll('.number'); // stores all .number button elements in numButtons
+console.dir(numButtons); 
 
 /*-------------------------------- Variables --------------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
 
 /*----------------------------- Event Listeners -----------------------------*/
+numButtons.forEach(numBtn => {
+    numBtn.addEventListener('click', handleNumClick);
+});
 
 /*-------------------------------- Functions --------------------------------*/
+function handleNumClick(event) {
+    console.log(event.target.innerText);
+}
