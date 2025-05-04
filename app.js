43747b55833eb8f771ab04c40364423b6cfe0e9d
User Name <landonisaiah@gmail.com>
2025-05-04 16:46:42 -0700

    display number on calculator screen

--- app.js
+++ app.js
@@ -1,17 +1,25 @@
 /*-------------------------------- Constants --------------------------------*/
-const numButtons = document.querySelectorAll('.number'); // stores all .number button elements in numButtons
-console.dir(numButtons); 
+
+const numButtons = document.querySelectorAll('.number'); // stores all .number button elements in 'numButtons'
+// console.dir(numButtons); 
 
 /*-------------------------------- Variables --------------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
 
+const display = document.querySelector('.display'); // stores div.display element in 'display'
+console.dir(display);
+
 /*----------------------------- Event Listeners -----------------------------*/
+
 numButtons.forEach(numBtn => {
     numBtn.addEventListener('click', handleNumClick);
 });
 
 /*-------------------------------- Functions --------------------------------*/
+
 function handleNumClick(event) {
-    console.log(event.target.innerText);
+    // console.log(event.target.innerText);
+    // console.log(parseInt(event.target.innerText));  // logs the innerText converted to a number
+    display.innerText = parseInt(event.target.innerText); // inserts the converted number into the div.display element, shows on calculator screen
 }
