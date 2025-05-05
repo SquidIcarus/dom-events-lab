be166580a5fec993fef39557b6376c9fc0cc9649
User Name <landonisaiah@gmail.com>
2025-05-04 17:16:30 -0700

    operator function if...else to clear with 'C' button

--- app.js
+++ app.js
@@ -3,13 +3,14 @@
 const numButtons = document.querySelectorAll('.number'); // stores all .number button elements in 'numButtons'
 const operButtons = document.querySelectorAll('.operator'); // stores all button.operator elements in 'operButtons'
 // console.dir(numButtons); 
-console.dir(operButtons);
+// console.dir(operButtons);
 
 /*-------------------------------- Variables --------------------------------*/
 
 /*------------------------ Cached Element References ------------------------*/
 
 const display = document.querySelector('.display'); // stores div.display element in 'display'
+display.innerText = '0';                            // display shows '0' until a number button is pressed
 // console.dir(display);
 
 /*----------------------------- Event Listeners -----------------------------*/
@@ -31,5 +32,11 @@ function handleNumClick(event) {                       // callback function for 
 }
 
 function handleOperClick(event) {                      // callback function for 'operButtons' eventListener
-    console.log(event.target.innerText);
+    const operator = event.target.innerText;           // stores the innerText of button pressed in 'operator'
+    // console.log(operator);
+    if (operator === 'C') {                            // if 'C' button is pressed
+        display.innerText = '0';                       // clear the screen and display '0' 
+    } else {                                           // otherwise...         
+        console.log(operator);                         // log the 'operator'
+    }
 }
